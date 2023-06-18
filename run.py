c030413142e14736ffcef55faec536e2699eaaa9
Mauro Saavedra <saa.mauro92@gmail.com>
2023-06-18 21:00:55 +0000

    Create recipeClass with name, instructions and print_data method

--- run.py
+++ run.py
@@ -1,4 +1,19 @@
 
+class recipeClass:
+    def __init__(self, name, instructions):
+        self.name = name
+        self.instructions = instructions
+
+    def print_data(self):
+        return f"""
+        {self.name}\n \n
+
+        Instructions: \n \n
+        {self.instructions}
+
+
+        """
+
 
 def display_program_welcome():
     """
@@ -19,6 +34,8 @@ def display_program_welcome():
 def main():
     display_program_welcome()
 
+    new_recipe = recipeClass("fake recipe", "fake instructions...")
+    print(new_recipe.print_data())
 
 
 main()
