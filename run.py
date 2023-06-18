72cd787befbcadfa20d289ce2dc0dc135889cf34
Mauro Saavedra <saa.mauro92@gmail.com>
2023-06-18 20:25:36 +0000

    Add main function and display_program_welcome that prints the welcome to the program text

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+def display_program_welcome():
+    """
+    Prints 'welcome to the reci-python program'
+    """
+    print(
+        """
+              WELCOME TO
+
+ █▀▄ ██▀ ▄▀▀ █    █▀▄ ▀▄▀ ▀█▀ █▄█ ▄▀▄ █▄ █
+ █▀▄ █▄▄ ▀▄▄ █ ▀▀ █▀   █   █  █ █ ▀▄▀ █ ▀█
+
+             (RECI-PYTHON)
+
+    """)
+
+
+def main():
+    display_program_welcome()
+
+
+
+main()
