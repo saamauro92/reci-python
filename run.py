b8f459b857f1f56a2618f18acbd2d345d14801b2
mauro saavedra <saa.mauro92@gmail.com>
2023-06-26 20:43:05 +0100

    Fix text to pass validator in display_more_options()

--- run.py
+++ run.py
@@ -125,11 +125,10 @@ def display_more_options():
     Displays an input and returns the value options 1, 2 or 3
     """
     while True:
-        option_selected = input(f"""
-        \n    {Fore.CYAN}  -->   {Style.RESET_ALL} Select options:
-      1 To add another ingredient     3 To start again
-      2 To get recipe
-\n""")
+        option_selected = input(
+           f"\n{Fore.CYAN}-->{Style.RESET_ALL} Select options:\n" +
+           "      1 To add another ingredient     3 To start again\n" +
+           "      2 To get recipe \n")
         if option_selected == '1':
             return 1
         elif option_selected == '2':
